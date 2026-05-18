c68e2250f05ef0e971357129d7519e18f3c51f1b
gnz-e <94329797+gnz-e@users.noreply.github.com>
2026-05-18 14:52:10 -0600

    Add alternative solution for SQL exercise 09

--- exercise09/exercise09.sql
+++ exercise09/exercise09.sql
@@ -110,3 +110,24 @@ WHERE em.salary = (
     FROM Employee
     WHERE departmentId = dp.id
 );
+
+SELECT
+    Department,
+    Employee,
+    Salary
+FROM (
+    SELECT
+        d.name AS Department,
+        e.name AS Employee,
+        e.salary AS Salary,
+
+        DENSE_RANK() OVER (
+            PARTITION BY e.departmentId
+            ORDER BY e.salary DESC
+        ) AS ranking
+
+    FROM Employee e
+    INNER JOIN Department d
+        ON e.departmentId = d.id
+) ranked
+WHERE ranking = 1;
